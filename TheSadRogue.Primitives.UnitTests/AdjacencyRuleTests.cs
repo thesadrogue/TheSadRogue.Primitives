aa28682c606ec46e98c8511d54c680c237e40e51
Chris3606 <cridley106@gmail.com>
2019-11-11 19:19:03 -0600

    Fixed namespaces that were wrong (as well as the default namespaces for projects).  Added helper functions for MonoGame color to MonoGame extension.

--- TheSadRogue.Primitives.UnitTests/AdjacencyRuleTests.cs
+++ TheSadRogue.Primitives.UnitTests/AdjacencyRuleTests.cs
@@ -5,7 +5,7 @@ using SadRogue.Primitives;
 using Xunit;
 using XUnit.ValueTuples;
 
-namespace TheSadRogue.Primitives.UnitTests
+namespace SadRogue.Primitives.UnitTests
 {
     public class AdjacencyRuleTests
     {

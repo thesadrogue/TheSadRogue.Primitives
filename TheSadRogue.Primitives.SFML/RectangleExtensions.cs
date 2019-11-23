189d3e827c813899873d6be2a378cf1f889157b4
SadConsole Master <sadconsole@outlook.com>
2019-11-23 13:35:22 -0800

    Fix SFML rect

--- TheSadRogue.Primitives.SFML/RectangleExtensions.cs
+++ TheSadRogue.Primitives.SFML/RectangleExtensions.cs
@@ -5,7 +5,7 @@ namespace SadRogue.Primitives
 {
     public static class RectangleExtensions
     {
-        public static IntRect ToIntRect(this SadRogueRectangle self) => new IntRect(self.X, self.Y, self.Width, self.Height);
+        public static IntRect ToIntRect(this SadRogueRectangle self) => new IntRect(self.X, self.Y, self.X + self.Width, self.Y + self.Height);
 
         public static bool Equals(this SadRogueRectangle self, IntRect other)
             => self.X == other.Left && self.Y == other.Top && self.Width == other.Width && self.Height == other.Height;
@@ -16,7 +16,7 @@ namespace SFML.Graphics
 {
     public static class IntRectExtensions
     {
-        public static SadRogueRectangle ToRectangle(this IntRect self) => new SadRogueRectangle(self.Left, self.Top, self.Width, self.Height);
+        public static SadRogueRectangle ToRectangle(this IntRect self) => new SadRogueRectangle(self.Left, self.Top, self.Width - self.Left, self.Height - self.Top);
 
         public static bool Equals(this IntRect self, SadRogueRectangle other)
             => self.Left == other.X && self.Top == other.Y && self.Width == other.Width && self.Height == other.Height;

2ed871879c3645a3631323a52c5ad5c64c886797
Chris3606 <cridley106@gmail.com>
2020-05-23 10:08:16 -0500

    Updated nuget deployment script to work for this package.

--- nuget/push.bat
+++ nuget/push.bat
@@ -1,2 +1,2 @@
-nuget push GoRogue.%1-debug.nupkg -Source https://api.nuget.org/v3/index.json
-nuget push GoRogue.%1.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push TheSadRogue.Primitives.%1-debug.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push TheSadRogue.Primitives.%1.nupkg -Source https://api.nuget.org/v3/index.json

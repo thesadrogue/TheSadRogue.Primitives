dee3740055100dfaf5cb092c63a03ef4955b2752
Chris3606 <cridley106@gmail.com>
2020-11-16 21:21:05 -0600

    Added grid views from GoRogue.

---
+++ TheSadRogue.Primitives/GridViews/GridViewBase.cs
@@ -0,0 +1,26 @@
+﻿namespace SadRogue.Primitives.GridViews
+{
+    /// <summary>
+    /// A convenient base class to inherit from when implementing <see cref="IGridView{T}"/> that minimizes
+    /// the number of items you must implement by implementing indexers in terms of a single indexer taking a Point.
+    /// </summary>
+    public abstract class GridViewBase<T> : IGridView<T>
+    {
+        /// <inheritdoc />
+        public abstract int Height { get; }
+        /// <inheritdoc />
+        public abstract int Width { get; }
+
+        /// <inheritdoc />
+        public abstract T this[Point pos] { get; }
+
+        /// <inheritdoc />
+        public int Count => Width * Height;
+
+        /// <inheritdoc />
+        public T this[int x, int y] => this[new Point(x, y)];
+
+        /// <inheritdoc />
+        public T this[int index1D] => this[Point.FromIndex(index1D, Width)];
+    }
+}

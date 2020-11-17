9d8ee73cbd3d1870655f324e75da5c654f6ef1d5
Chris3606 <cridley106@gmail.com>
2020-11-16 22:04:47 -0600

    Added basic grid view unit tests.

---
+++ TheSadRogue.Primitives.UnitTests/Mocks/MockGridViews.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SadRogue.Primitives.GridViews;
+
+namespace SadRogue.Primitives.UnitTests.Mocks
+{
+    /// <summary>
+    /// A class that creates mock grid views for use in unit tests.
+    /// </summary>
+    internal static class MockGridViews
+    {
+        public static ISettableGridView<double> RandomDoubleGrid(int width, int height)
+        {
+            var grid = new ArrayView<double>(width, height);
+            Random rng = new Random();
+
+            foreach (var pos in grid.Positions())
+                grid[pos] = rng.NextDouble();
+
+            return grid;
+        }
+
+        public static ISettableGridView<bool> RectangleBooleanGrid(int width, int height)
+        {
+            ISettableGridView<bool> grid = new ArrayView<bool>(width, height);
+            foreach (var pos in grid.Bounds().Expand(-1, -1).Positions())
+                grid[pos] = true;
+
+            return grid;
+        }
+    }
+}

17d43ce098f94f53f628b0320bbf54f69a9a926a
Chris3606 <cridley106@gmail.com>
2021-08-20 21:04:57 -0500

    Removed unneeded using statement.

--- TheSadRogue.Primitives/Area.cs
+++ TheSadRogue.Primitives/Area.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 using System.Text;
 
 namespace SadRogue.Primitives

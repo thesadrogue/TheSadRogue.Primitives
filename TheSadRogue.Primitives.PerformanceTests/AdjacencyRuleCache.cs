c9d4f5c50ce7e4bea64605ef68f6c68db53106cd
Chris3606 <cridley106@gmail.com>
2022-01-08 12:59:07 -0600

    Added performance tests to highlight the benefit of caching results.

---
+++ TheSadRogue.Primitives.PerformanceTests/AdjacencyRuleCache.cs
@@ -0,0 +1,83 @@
+﻿using BenchmarkDotNet.Attributes;
+using SadRogue.Primitives;
+
+namespace TheSadRogue.Primitives.PerformanceTests
+{
+    /// <summary>
+    /// Performance tests demonstrating the benefit of keeping cached versions of DirectionsOfNeighbors type functions
+    /// in AdjacencyRule
+    /// </summary>
+    public class AdjacencyRuleCache
+    {
+        [ParamsAllValues]
+        public AdjacencyRule.Types RuleType;
+
+        private AdjacencyRule _rule;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _rule = RuleType;
+        }
+
+        [Benchmark]
+        public int NeighborsViaEnumerable()
+        {
+            int sum = 0;
+            foreach (var dir in _rule.DirectionsOfNeighbors())
+                sum += (int)dir.Type;
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int NeighborsViaCache()
+        {
+            int sum = 0;
+            for (int i = 0; i < _rule.DirectionsOfNeighborsCache.Length; i++)
+                sum += (int)_rule.DirectionsOfNeighborsCache[i].Type;
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int NeighborsClockwiseViaEnumerable()
+        {
+            int sum = 0;
+            foreach (var dir in _rule.DirectionsOfNeighborsClockwise())
+                sum += (int)dir.Type;
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int NeighborsClockwiseViaCache()
+        {
+            int sum = 0;
+            for (int i = 0; i < _rule.DirectionsOfNeighborsClockwiseCache.Length; i++)
+                sum += (int)_rule.DirectionsOfNeighborsClockwiseCache[i].Type;
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int NeighborsCounterClockwiseViaEnumerable()
+        {
+            int sum = 0;
+            foreach (var dir in _rule.DirectionsOfNeighborsCounterClockwise())
+                sum += (int)dir.Type;
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int NeighborsCounterClockwiseViaCache()
+        {
+            int sum = 0;
+            for (int i = 0; i < _rule.DirectionsOfNeighborsCounterClockwiseCache.Length; i++)
+                sum += (int)_rule.DirectionsOfNeighborsCounterClockwiseCache[i].Type;
+
+            return sum;
+        }
+    }
+}

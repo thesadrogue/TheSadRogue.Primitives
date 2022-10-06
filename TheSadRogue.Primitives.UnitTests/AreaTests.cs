40cc2bbca91d0c90f7a243ab9030edc8846cdcd9
Chris3606 <cridley106@gmail.com>
2022-10-05 19:19:59 -0500

    Added unit tests to validate PerimeterPositions function.

--- TheSadRogue.Primitives.UnitTests/AreaTests.cs
+++ TheSadRogue.Primitives.UnitTests/AreaTests.cs
@@ -33,13 +33,75 @@ namespace SadRogue.Primitives.UnitTests
             var l4 = new List<Point>();
             foreach (var pos in area.FastEnumerator())
                 l4.Add(pos);
-            
+
 
             Assert.Equal(expected, l1);
             Assert.Equal(expected, l2);
             Assert.Equal(expected, l3);
             Assert.Equal(expected, l4);
+        }
 
+        [Fact]
+        public void RectangleAreaPerimeterPositions()
+        {
+            var rect = new Rectangle(0, 0, 15, 15);
+            var area = new Area(rect.Positions().ToEnumerable());
+
+            var expected = rect.PerimeterPositions().ToHashSet();
+            var actual1 = area.PerimeterPositions(AdjacencyRule.Cardinals).ToHashSet();
+            var actual2 = area.PerimeterPositions(AdjacencyRule.EightWay).ToHashSet();
+
+            Assert.Equal(expected, actual1);
+            Assert.Equal(expected, actual2);
+        }
+
+        // X _not_ considered a border, by four-way adjacency
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . X .
+        // . . . . . . . #
+        [Fact]
+        public void IrregularAreaPerimeterPositionsCardinals()
+        {
+            var rect = new Rectangle(0, 0, 15, 15);
+            var area = new Area(rect.Positions().ToEnumerable());
+            area.Remove(rect.MaxExtent);
+
+            var expected = rect.PerimeterPositions().ToHashSet();
+            expected.Remove(rect.MaxExtent);
+
+            var actual = area.PerimeterPositions(AdjacencyRule.Cardinals).ToHashSet();
+
+            Assert.Equal(expected, actual);
+        }
+
+        // X _is_ considered a border, by eight-way adjacency
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . . .
+        // . . . . . . X .
+        // . . . . . . . #
+        [Fact]
+        public void IrregularAreaPerimeterPositionsEightWay()
+        {
+            var rect = new Rectangle(0, 0, 15, 15);
+            var area = new Area(rect.Positions().ToEnumerable());
+            area.Remove(rect.MaxExtent);
+
+            var expected = rect.PerimeterPositions().ToHashSet();
+            expected.Remove(rect.MaxExtent);
+            expected.Add(rect.MaxExtent - 1);
+
+            var actual = area.PerimeterPositions(AdjacencyRule.EightWay).ToHashSet();
+
+            Assert.Equal(expected, actual);
         }
     }
 }

722b009fc5237b0bda2272d68bec4de0fd31973b
Chris3606 <cridley106@gmail.com>
2022-10-09 17:06:22 -0500

    Added performance tests indicating potential performance gains.

---
+++ TheSadRogue.Primitives.PerformanceTests/DistanceOverloads.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using SadRogue.Primitives;
+
+namespace TheSadRogue.Primitives.PerformanceTests
+{
+    public class DistanceOverloads
+    {
+        [Params(4)]
+        public int DeltaX;
+
+        [Params(7)]
+        public int DeltaY;
+
+        //[ParamsAllValues]
+        //public Distance.Types DistanceType;
+
+        [Benchmark]
+        public double ChebyshevDouble()
+        {
+            double dx = Math.Abs(DeltaX);
+            double dy = Math.Abs(DeltaY);
+
+            return Math.Max(dx, dy);
+        }
+
+        [Benchmark]
+        public double ChebyshevDoubleNoNaN()
+        {
+            double dx = Math.Abs(DeltaX);
+            double dy = Math.Abs(DeltaY);
+
+            return dx > dy ? dx : dy;
+        }
+
+        [Benchmark]
+        public double ChebyshevInt()
+        {
+            int dx = Math.Abs(DeltaX);
+            int dy = Math.Abs(DeltaY);
+
+            return Math.Max(dx, dy);
+        }
+    }
+}

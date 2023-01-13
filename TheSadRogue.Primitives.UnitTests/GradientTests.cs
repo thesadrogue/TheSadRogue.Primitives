f62995a95186b4b971d40e2608128c28b3e35a82
Chris3606 <cridley106@gmail.com>
2023-01-12 21:54:31 -0600

    Added Gradient constructor tests.  Corrected exception type thrown in Gradient constructor to meet library standard.

--- TheSadRogue.Primitives.UnitTests/GradientTests.cs
+++ TheSadRogue.Primitives.UnitTests/GradientTests.cs
@@ -1,6 +1,9 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
 using XUnit.ValueTuples;
 using System.Linq;
+using System.Runtime.Loader;
 
 namespace SadRogue.Primitives.UnitTests
 {
@@ -66,6 +69,104 @@ namespace SadRogue.Primitives.UnitTests
 
     public class GradientTests
     {
+        #region Constructors
 
+        [Fact]
+        public void TwoColorConstructor()
+        {
+            var gradient = new Gradient(Color.Aqua, Color.Red);
+            Assert.Equal(2, gradient.Stops.Length);
+
+            Assert.Equal(Color.Aqua, gradient.Stops[0].Color);
+            Assert.Equal(0f, gradient.Stops[0].Stop);
+
+            Assert.Equal(Color.Red, gradient.Stops[1].Color);
+            Assert.Equal(1f, gradient.Stops[1].Stop);
+        }
+
+        [Fact]
+        public void EnumerableOfStopsConstructor()
+        {
+            var stops = new[]
+            {
+                new GradientStop(Color.Aqua, 0f), new GradientStop(Color.Orange, .4f),
+                new GradientStop(Color.White, 1f)
+            };
+
+            var gradient = new Gradient(stops);
+            Assert.Equal((IEnumerable<GradientStop>)stops, gradient.Stops);
+        }
+
+        [Fact]
+        public void MultipleEnumerableConstructor()
+        {
+            var colors = new[] { Color.Aqua, Color.Orange, Color.White };
+            float[] stops = { 0f, .4f, 1f };
+
+            var gradient = new Gradient(colors, stops);
+
+            Assert.Equal(colors.Length, gradient.Stops.Length);
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var expectedStop = new GradientStop(colors[i], stops[i]);
+                Assert.Equal(expectedStop, gradient.Stops[i]);
+            }
+        }
+
+        [Fact]
+        public void MultipleEnumerableConstructorMismatchedSizes()
+        {
+            var colors = new[] { Color.Aqua, Color.Orange, Color.White };
+            float[] stops = { 0f, 1f };
+
+            Assert.Throws<ArgumentException>(() => new Gradient(colors, stops));
+
+            stops = new[] { 0f, .4f, .5f, 1f };
+            Assert.Throws<ArgumentException>(() => new Gradient(colors, stops));
+        }
+
+        [Fact]
+        public void EvenlySpaceColorsConstructor()
+        {
+            var initializationColors = new[] { Color.Aqua, Color.Orange, Color.White };
+
+            // This is a params constructor, but we can call it this way too
+            var gradient = new Gradient(initializationColors);
+
+            Assert.Equal(initializationColors.Length, gradient.Stops.Length);
+
+            float increment = 1f / (initializationColors.Length - 1);
+            float expectedStop = 0f;
+            for (int i = 0; i < gradient.Stops.Length; i++)
+            {
+                var currentStop = gradient.Stops[i];
+                Assert.Equal(initializationColors[i], currentStop.Color);
+                Assert.Equal(expectedStop, currentStop.Stop);
+
+                expectedStop += increment;
+            }
+        }
+
+        [Fact]
+        public void EvenlySpaceColorsSingleColor()
+        {
+            var gradient = new Gradient(Color.Green);
+
+            Assert.Equal(2, gradient.Stops.Length);
+
+            Assert.Equal(Color.Green, gradient.Stops[0].Color);
+            Assert.Equal(0f, gradient.Stops[0].Stop);
+
+            Assert.Equal(Color.Green, gradient.Stops[1].Color);
+            Assert.Equal(1f, gradient.Stops[1].Stop);
+        }
+
+        [Fact]
+        public void EvenlySpacedColorsConstructorNoColors()
+        {
+            var colors = Array.Empty<Color>();
+            Assert.Throws<ArgumentException>(() => new Gradient(colors));
+        }
+        #endregion
     }
 }

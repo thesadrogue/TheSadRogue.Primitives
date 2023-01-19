80a3d591dd9b763d544970d655fe22e324ca12e1
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-01-18 18:58:39 -0800

    Just in case, BareMinimumAlgorithm.
    This is an extremely simple algorithm, and could still be adapted for more SadRogue-specific usage. It just adds X to a bitwise-rotated Y. Changing the  return line to `return (int)(x + (y << 8 | y >> 24));` may be a good choice for the specific requirements of a hash code here, since it makes every cell in a 256x256 grid have a unique 16-bit hash code. This may not matter because Dictionary and HashSet use prime modulus tables.

---
+++ TheSadRogue.Primitives.PerformanceTests/PointHashing/Algorithms/BareMinimumAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using SadRogue.Primitives;
+
+namespace TheSadRogue.Primitives.PerformanceTests.PointHashing.Algorithms
+{
+    /// <summary>
+    /// Algorithm using the simplest function I could think of.
+    /// </summary>
+    public sealed class BareMinimumAlgorithm : EqualityComparer<Point>
+    {
+        public static readonly IEqualityComparer<Point> Instance = new BareMinimumAlgorithm();
+
+        public override bool Equals(Point x, Point y) => x.Equals(y);
+
+        public override int GetHashCode(Point p)
+        {
+            uint x = (uint)p.X, y = (uint)p.Y;
+            return (int)(x + (y << 16 | y >> 16));
+        }
+    }
+}

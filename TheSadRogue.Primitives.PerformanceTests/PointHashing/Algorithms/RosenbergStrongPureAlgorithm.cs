867ea9fb146b1cbe5af422a67da38d56f52acead
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-01-18 18:52:53 -0800

    Add RosenbergStrongPureAlgorithm.
    This is just like the existing Rosenberg-Strong-based algorithms, but doesn't add 3 to X or Y, and doesn't randomize afterward at all. An advantage here is that every cell in a 256x256 grid is guaranteed to have a unique 16-bit hash code. A disadvantage is that negative X or Y aren't handled well.

---
+++ TheSadRogue.Primitives.PerformanceTests/PointHashing/Algorithms/RosenbergStrongPureAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using SadRogue.Primitives;
+
+namespace TheSadRogue.Primitives.PerformanceTests.PointHashing.Algorithms
+{
+    /// <summary>
+    /// Algorithm using the exact Rosenberg-Strong pairing function.
+    /// This does not provide special treatment for negative inputs.
+    /// </summary>
+    public sealed class RosenbergStrongPureAlgorithm : EqualityComparer<Point>
+    {
+        public static readonly IEqualityComparer<Point> Instance = new RosenbergStrongPureAlgorithm();
+
+        public override bool Equals(Point x, Point y) => x.Equals(y);
+
+        public override int GetHashCode(Point p)
+        {
+            int x = p.X, y = p.Y;
+            return (x >= y ? x * (x + 2) - y : y * y + x);
+        }
+    }
+}

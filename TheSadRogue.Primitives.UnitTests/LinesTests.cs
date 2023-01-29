74c78c8ba2ba4fe8347a8815eb734b9b20dc1e8f
Chris3606 <cridley106@gmail.com>
2023-01-28 20:03:50 -0600

    Added initial tests indicating that custom enumerables are significantly faster for lines.

--- TheSadRogue.Primitives.UnitTests/LinesTests.cs
+++ TheSadRogue.Primitives.UnitTests/LinesTests.cs
@@ -6,6 +6,186 @@ using XUnit.ValueTuples;
 
 namespace SadRogue.Primitives.UnitTests
 {
+
+    public static class SimpleAlgorithms
+    {
+        private const int ModifierX = 0x7fff;
+        private const int ModifierY = 0x7fff;
+
+        private static IEnumerable<Point> Bresenham(int startX, int startY, int endX, int endY)
+        {
+            int w = endX - startX;
+            int h = endY - startY;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1;
+            else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1;
+            else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1;
+            else if (w > 0) dx2 = 1;
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1;
+                else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+
+            int numerator = longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
+                yield return new Point(startX, startY);
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    startX += dx1;
+                    startY += dy1;
+                }
+                else
+                {
+                    startX += dx2;
+                    startY += dy2;
+                }
+            }
+        }
+
+        private static IEnumerable<Point> DDA(int startX, int startY, int endX, int endY)
+        {
+            int dx = endX - startX;
+            int dy = endY - startY;
+
+            int nx = Math.Abs(dx);
+            int ny = Math.Abs(dy);
+
+            // Calculate octant value
+            int octant = (dy < 0 ? 4 : 0) | (dx < 0 ? 2 : 0) | (ny > nx ? 1 : 0);
+            int fraction = 0;
+            int mn = Math.Max(nx, ny);
+
+            int move;
+
+            if (mn == 0)
+            {
+                yield return new Point(startX, startY);
+                yield break;
+            }
+
+            if (ny == 0)
+            {
+                if (dx > 0)
+                    for (int x = startX; x <= endX; x++)
+                        yield return new Point(x, startY);
+                else
+                    for (int x = startX; x >= endX; x--)
+                        yield return new Point(x, startY);
+
+                yield break;
+            }
+
+            if (nx == 0)
+            {
+                if (dy > 0)
+                    for (int y = startY; y <= endY; y++)
+                        yield return new Point(startX, y);
+                else
+                    for (int y = startY; y >= endY; y--)
+                        yield return new Point(startX, y);
+
+                yield break;
+            }
+
+            switch (octant)
+            {
+                case 0: // +x, +y
+                    move = (ny << 16) / nx;
+                    for (int primary = startX; primary <= endX; primary++, fraction += move)
+                        yield return new Point(primary, startY + ((fraction + ModifierY) >> 16));
+                    break;
+
+                case 1:
+                    move = (nx << 16) / ny;
+                    for (int primary = startY; primary <= endY; primary++, fraction += move)
+                        yield return new Point(startX + ((fraction + ModifierX) >> 16), primary);
+                    break;
+
+                case 2: // -x, +y
+                    move = (ny << 16) / nx;
+                    for (int primary = startX; primary >= endX; primary--, fraction += move)
+                        yield return new Point(primary, startY + ((fraction + ModifierY) >> 16));
+                    break;
+
+                case 3:
+                    move = (nx << 16) / ny;
+                    for (int primary = startY; primary <= endY; primary++, fraction += move)
+                        yield return new Point(startX - ((fraction + ModifierX) >> 16), primary);
+                    break;
+
+                case 6: // -x, -y
+                    move = (ny << 16) / nx;
+                    for (int primary = startX; primary >= endX; primary--, fraction += move)
+                        yield return new Point(primary, startY - ((fraction + ModifierY) >> 16));
+                    break;
+
+                case 7:
+                    move = (nx << 16) / ny;
+                    for (int primary = startY; primary >= endY; primary--, fraction += move)
+                        yield return new Point(startX - ((fraction + ModifierX) >> 16), primary);
+                    break;
+
+                case 4: // +x, -y
+                    move = (ny << 16) / nx;
+                    for (int primary = startX; primary <= endX; primary++, fraction += move)
+                        yield return new Point(primary, startY - ((fraction + ModifierY) >> 16));
+                    break;
+
+                case 5:
+                    move = (nx << 16) / ny;
+                    for (int primary = startY; primary >= endY; primary--, fraction += move)
+                        yield return new Point(startX + ((fraction + ModifierX) >> 16), primary);
+                    break;
+            }
+        }
+
+        private static IEnumerable<Point> Ortho(int startX, int startY, int endX, int endY)
+        {
+            int dx = endX - startX;
+            int dy = endY - startY;
+
+            int nx = Math.Abs(dx);
+            int ny = Math.Abs(dy);
+
+            int signX = dx > 0 ? 1 : -1;
+            int signY = dy > 0 ? 1 : -1;
+
+            int workX = startX;
+            int workY = startY;
+
+            yield return new Point(startX, startY);
+
+            for (int ix = 0, iy = 0; ix < nx || iy < ny;)
+            {
+                // Optimized version of `if ((0.5 + ix) / nx < (0.5 + iy) / ny)`
+                if ((1 + ix + ix) * ny < (1 + iy + iy) * nx)
+                {
+                    workX += signX;
+                    ix++;
+                }
+                else
+                {
+                    workY += signY;
+                    iy++;
+                }
+
+                yield return new Point(workX, workY);
+            }
+        }
+    }
+
+
     public class LinesTests
     {
         #region TestData

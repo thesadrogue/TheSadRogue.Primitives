a90e7a6f9b9c486a0d667138b7ee9268d17a8d13
Chris3606 <cridley106@gmail.com>
2023-01-29 10:42:34 -0600

    Implemented ellipse via custom iterator.

--- TheSadRogue.Primitives.PerformanceTests/ShapeTests.cs
+++ TheSadRogue.Primitives.PerformanceTests/ShapeTests.cs
@@ -48,15 +48,51 @@ internal static class YieldReturnEnumerableShapes
                 err += ++xi * 2 + 1; /* e_xy+e_x > 0 or no 2nd y-step */
         } while (xi < 0);
     }
+
+    public static IEnumerable<Point> GetEllipse(int x0, int y0, int x1, int y1)
+    {
+        int a = Math.Abs(x1 - x0), b = Math.Abs(y1 - y0), b1 = b & 1; /* values of diameter */
+        long dx = 4 * (1 - a) * b * b, dy = 4 * (b1 + 1) * a * a; /* error increment */
+        long err = dx + dy + b1 * a * a, e2; /* error of 1.step */
+
+        if (x0 > x1) { x0 = x1; x1 += a; } /* if called with swapped points */
+        if (y0 > y1)
+        {
+            y0 = y1; /* .. exchange them */
+        }
+
+        y0 += (b + 1) / 2; y1 = y0 - b1;   /* starting pixel */
+        a *= 8 * a; b1 = 8 * b * b;
+
+        do
+        {
+            yield return new Point(x1, y0); /*   I. Quadrant */
+            yield return new Point(x0, y0); /*  II. Quadrant */
+            yield return new Point(x0, y1); /* III. Quadrant */
+            yield return new Point(x1, y1); /*  IV. Quadrant */
+            e2 = 2 * err;
+            if (e2 <= dy) { y0++; y1--; err += dy += a; }  /* y step */
+            if (e2 >= dx || 2 * err > dy) { x0++; x1--; err += dx += b1; } /* x step */
+        } while (x0 <= x1);
+
+        while (y0 - y1 < b)
+        {  /* too early stop of flat ellipses a=1 */
+            yield return new Point(x0 - 1, y0); /* -> finish tip of ellipse */
+            yield return new Point(x1 + 1, y0++);
+            yield return new Point(x0 - 1, y1);
+            yield return new Point(x1 + 1, y1--);
+        }
+    }
 }
 
-public class ShapeTests
+public class CircleTests
 {
     public static readonly Point Center = new (1, 1);
 
     [Params(5, 10, 25, 50)]
     public int Radius;
 
+    #region Circle
     [Benchmark]
     public int PrimitivesCircle()
     {
@@ -96,5 +132,51 @@ public class ShapeTests
             sum += point.X + point.Y;
 
         return sum;
+    }
+
+    #endregion
+}
+
+public class EllipseTests
+{
+    [ParamsSource(nameof(TestCases))]
+    public (Point f1, Point f2) Ellipse;
+
+    [Benchmark]
+    public int PrimitivesEllipse()
+    {
+        int sum = 0;
+        foreach (var point in Shapes.GetEllipse(Ellipse.f1, Ellipse.f2))
+            sum += point.X + point.Y;
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int PrimitivesEllipseToEnumerable()
+    {
+        int sum = 0;
+        foreach (var point in Shapes.GetEllipse(Ellipse.f1, Ellipse.f2).ToEnumerable())
+            sum += point.X + point.Y;
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int YieldReturnEllipse()
+    {
+        int sum = 0;
+        foreach (var point in YieldReturnEnumerableShapes.GetEllipse(Ellipse.f1.X, Ellipse.f1.Y, Ellipse.f2.X, Ellipse.f2.Y))
+            sum += point.X + point.Y;
+
+        return sum;
+    }
+
+    public IEnumerable<(Point f1, Point f2)> TestCases()
+    {
+        yield return ((1, 1), (25, 50));
+        yield return ((25, 50), (1, 1));
+        yield return ((1, 1), (50, 1));
+        yield return ((1, 1), (1, 25));
     }
 }
--- TheSadRogue.Primitives/Shapes.cs
+++ TheSadRogue.Primitives/Shapes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace SadRogue.Primitives
@@ -133,12 +134,185 @@ namespace SadRogue.Primitives
                     _err += ++_xi * 2 + 1; /* e_xy+e_x > 0 or no 2nd y-step */
 
             } while (_xi < 0);
+
+            _terminate = true;
         }
     }
 
     /// <summary>
-    /// Provides implementations of various line-drawing algorithms which are useful for
-    /// for generating lines on a 2D integer grid.
+    /// A custom enumerator used to iterate over all positions on the outside of an ellipse with a foreach loop efficiently.
+    /// Generally, you should use <see cref="Shapes.GetEllipse"/> to get an instance of this.
+    /// </summary>
+    /// <remarks>
+    /// This type is a struct, and as such is much more efficient than using the otherwise equivalent type of
+    /// IEnumerable&lt;Point&gt; with "yield return".  The type does contain a function <see cref="ToEnumerable"/> which
+    /// creates an IEnumerable&lt;Point&gt;, which can be convenient for allowing the returned positions to be used with
+    /// LINQ; however using this function is not recommended in situations where runtime performance is a primary
+    /// concern.
+    /// </remarks>
+    public struct EllipsePositionsEnumerable
+    {
+        // Suppress warning stating to use auto-property because we want to guarantee micro-performance
+        // characteristics.
+        #pragma warning disable IDE0032 // Use auto property
+        private Point _current;
+        #pragma warning restore IDE0032 // Use auto property
+
+        /// <summary>
+        /// The current value for enumeration.
+        /// </summary>
+        public Point Current => _current;
+
+        private long _dx;
+        private long _dy;
+        private readonly int _a;
+        private readonly int _b;
+        private readonly int _b1;
+        private int _x0;
+        private int _y0;
+        private int _x1;
+        private int _y1;
+        private int _state;
+        private bool _terminate;
+        private long _err;
+
+        /// <summary>
+        /// Creates an enumerator which iterates over all positions on the outside of the given ellipse.
+        /// </summary>
+        /// <param name="f1">The first focus point of the ellipse.</param>
+        /// <param name="f2">The second focus point of the ellipse.</param>
+        public EllipsePositionsEnumerable(Point f1, Point f2)
+        {
+            (_x0, _y0) = f1;
+            (_x1, _y1) = f2;
+
+            _current = Point.None;
+            _state = 0;
+            _terminate = false;
+
+            _a = Math.Abs(_x1 - _x0);
+            _b = Math.Abs(_y1 - _y0);
+            _b1 = _b & 1; /* values of diameter */
+
+            /* error increment */
+            _dx = 4 * (1 - _a) * _b * _b;
+            _dy = 4 * (_b1 + 1) * _a * _a;
+
+            _err = _dx + _dy + _b1 * _a * _a; /* error of 1.step */
+
+            if (_x0 > _x1) { _x0 = _x1; _x1 += _a; } /* if called with swapped points */
+            if (_y0 > _y1)
+                _y0 = _y1; /* .. exchange them */
+
+            _y0 += (_b + 1) / 2; _y1 = _y0 - _b1;   /* starting pixel */
+            _a *= 8 * _a; _b1 = 8 * _b * _b;
+        }
+
+        /// <summary>
+        /// Advances the iterator to the next position.
+        /// </summary>
+        /// <returns>True if the a new position on the outside of the circle exists; false otherwise.</returns>
+        public bool MoveNext()
+        {
+            switch (_state)
+            {
+                case 0: /*   I. Quadrant */
+                    _current = new Point(_x1, _y0);
+                    _state++;
+                    return true;
+                case 1: /*  II. Quadrant */
+                    _current = new Point(_x0, _y0);
+                    _state++;
+                    return true;
+                case 2: /* III. Quadrant */
+                    _current = new Point(_x0, _y1);
+                    _state++;
+                    return true;
+                case 3: /*  IV. Quadrant */
+                    _current = new Point(_x1, _y1);
+                    long e2 = 2 * _err;
+                    if (e2 <= _dy) { _y0++; _y1--; _err += _dy += _a; }  /* y step */
+                    if (e2 >= _dx || 2 * _err > _dy) { _x0++; _x1--; _err += _dx += _b1; } /* x step */
+
+                    _state = _x0 <= _x1 ? 0 : 4;
+                    if (_state == 4)
+                        _terminate = _y0 - _y1 >= _b;
+                    return true;
+                case 4: /* cases 4-7, too early stop of flat ellipses a=1 */
+                    if (_terminate)
+                        return false;
+
+                    _current = new Point(_x0 - 1, _y0);
+                    _state++;
+                    return true;
+                case 5:
+                    _current = new Point(_x1 + 1, _y0++);
+                    _state++;
+                    return true;
+                case 6:
+                    _current = new Point(_x0 - 1, _y1);
+                    _state++;
+                    return true;
+                case 7:
+                    _current = new Point(_x1 + 1, _y1--);
+                    _state = 4;
+                    _terminate = _y0 - _y1 >= _b;
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns this enumerator.
+        /// </summary>
+        /// <returns>This enumerator.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public EllipsePositionsEnumerable GetEnumerator() => this;
+
+        /// <summary>
+        /// Converts the result of the enumerable to a <see cref="IEnumerable{T}"/>, which can be useful if you need
+        /// to use the result with LINQ.
+        /// </summary>
+        /// <remarks>
+        /// Note that this function advances the state of the enumerator, evaluating it to its fullest extent.  Also
+        /// note that it is NOT recommended to use this function in cases where performance is critical.
+        /// </remarks>
+        /// <returns>
+        /// An IEnumerable&lt;Point&gt; which iterates over all positions on the outside of the circle specified to this
+        /// enumerator.
+        /// </returns>
+        public IEnumerable<Point> ToEnumerable()
+        {
+            // This is a re-implementation of the ellipse algorithm implemented in MoveNext above, and is equivalent to:
+            // foreach (var pos in this)
+            //    yield return pos
+            //
+            // However, this is notably faster.
+            do
+            {
+                yield return new Point(_x1, _y0); /*   I. Quadrant */
+                yield return new Point(_x0, _y0); /*  II. Quadrant */
+                yield return new Point(_x0, _y1); /* III. Quadrant */
+                yield return new Point(_x1, _y1); /*  IV. Quadrant */
+                long e2 = 2 * _err;
+                if (e2 <= _dy) { _y0++; _y1--; _err += _dy += _a; }  /* y step */
+                if (e2 >= _dx || 2 * _err > _dy) { _x0++; _x1--; _err += _dx += _b1; } /* x step */
+            } while (_x0 <= _x1);
+
+            while (_y0 - _y1 < _b)
+            {  /* too early stop of flat ellipses a=1 */
+                yield return new Point(_x0 - 1, _y0); /* -> finish tip of ellipse */
+                yield return new Point(_x1 + 1, _y0++);
+                yield return new Point(_x0 - 1, _y1);
+                yield return new Point(_x1 + 1, _y1--);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Provides implementations of various shape algorithms which are useful for
+    /// for generating shapes on a 2D integer grid.
     /// </summary>
     public static class Shapes
     {
@@ -157,6 +331,19 @@ namespace SadRogue.Primitives
         public static CirclePositionsEnumerable GetCircle(Point center, int radius)
             => new CirclePositionsEnumerable(center, radius);
 
-        // TODO: Ellipse
+        /// <summary>
+        /// Gets the points on the outside of an ellipse.
+        /// </summary>
+        /// <remarks>
+        /// This function returns a custom iterator which is very fast when used in a foreach loop.
+        /// If you need an IEnumerable to use with LINQ or other code, you can take the result of this function and
+        /// call <see cref="CirclePositionsEnumerable.ToEnumerable()"/> on it; however note that iterating over
+        /// this will not perform as well as iterating directly over this object.
+        /// </remarks>
+        /// <param name="f1">The first focus point of the ellipse.</param>
+        /// <param name="f2">The second focus point of the ellipse.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EllipsePositionsEnumerable GetEllipse(Point f1, Point f2)
+            => new EllipsePositionsEnumerable(f1, f2);
     }
 }

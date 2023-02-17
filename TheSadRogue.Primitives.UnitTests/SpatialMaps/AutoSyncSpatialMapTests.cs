0ff2613bcae90f6a383eb4a48c2fa53dcc377f0d
Chris3606 <cridley106@gmail.com>
2023-02-17 13:19:56 -0600

    Added tests for AutoSyncMultiSpatialMap, and fixed a bug with Add.

---
+++ TheSadRogue.Primitives.UnitTests/SpatialMaps/AutoSyncSpatialMapTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using SadRogue.Primitives.SpatialMaps;
+using SadRogue.Primitives.UnitTests.Mocks;
+using Xunit;
+
+namespace SadRogue.Primitives.UnitTests.SpatialMaps
+{
+    public class AutoSyncSpatialMapTests
+    {
+        [Fact]
+        public void MovementAutoSyncs()
+        {
+            var map = new AutoSyncMultiSpatialMap<MockPositionableSpatialMapItem>();
+            var item = new MockPositionableSpatialMapItem(1, (1, 2));
+            map.Add(item);
+
+            Assert.Equal(1, map.Count);
+            Assert.Single(map.GetItemsAt(item.Position));
+
+            item.Position = (3, 4);
+
+            Assert.Equal(1, map.Count);
+            Assert.Empty(map.GetItemsAt((1, 2)));
+            Assert.Single(map.GetItemsAt(item.Position));
+
+            var item2 = new MockPositionableSpatialMapItem(1, (1, 2));
+            map.Add(item2);
+            item.Position = item2.Position;
+            Assert.Equal(2, map.Count);
+            Assert.Equal(2, map.GetItemsAt((1, 2)).Count());
+
+            map.Remove(item);
+            item.Position = (5, 6); // Validate event handler is unregistered
+            Assert.Equal(1, map.Count);
+        }
+
+    }
+}

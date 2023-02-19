28cb00b8d72845c9d3efd72cc87095cbcb9051f2
Chris3606 <cridley106@gmail.com>
2023-02-19 10:52:03 -0600

    Added unit tests for the auto-syncing variant of SpatialMap.

--- TheSadRogue.Primitives.UnitTests/SpatialMaps/AutoSyncSpatialMapTests.cs
+++ TheSadRogue.Primitives.UnitTests/SpatialMaps/AutoSyncSpatialMapTests.cs
@@ -1,16 +1,15 @@
-﻿using System.Linq;
-using SadRogue.Primitives.SpatialMaps;
+﻿using SadRogue.Primitives.SpatialMaps;
 using SadRogue.Primitives.UnitTests.Mocks;
 using Xunit;
 
 namespace SadRogue.Primitives.UnitTests.SpatialMaps
 {
-    public class AutoSyncMultiSpatialMapTests
+    public class AutoSyncSpatialMapTests
     {
         [Fact]
         public void SettingPositionAutoSyncsSpatialMap()
         {
-            var map = new AutoSyncMultiSpatialMap<MockPositionableSpatialMapItem>();
+            var map = new AutoSyncSpatialMap<MockPositionableSpatialMapItem>();
             var item = new MockPositionableSpatialMapItem(1, (1, 2));
             map.Add(item);
 
@@ -23,21 +22,15 @@ namespace SadRogue.Primitives.UnitTests.SpatialMaps
             Assert.Empty(map.GetItemsAt((1, 2)));
             Assert.Single(map.GetItemsAt(item.Position));
 
-            var item2 = new MockPositionableSpatialMapItem(1, (1, 2));
-            map.Add(item2);
-            item.Position = item2.Position;
-            Assert.Equal(2, map.Count);
-            Assert.Equal(2, map.GetItemsAt((1, 2)).Count());
-
             map.Remove(item);
             item.Position = (5, 6); // Validate event handler is unregistered
-            Assert.Equal(1, map.Count);
+            Assert.Equal(0, map.Count);
         }
 
         [Fact]
         public void UsingMoveFunctionsAutoSyncsPosition()
         {
-            var map = new AutoSyncMultiSpatialMap<MockPositionableSpatialMapItem>();
+            var map = new AutoSyncSpatialMap<MockPositionableSpatialMapItem>();
             var item = new MockPositionableSpatialMapItem(1, (1, 2));
             map.Add(item);
 
@@ -51,22 +44,15 @@ namespace SadRogue.Primitives.UnitTests.SpatialMaps
             Assert.Single(map.GetItemsAt(item.Position));
             Assert.Equal(new Point(3, 4), item.Position);
 
-            var item2 = new MockPositionableSpatialMapItem(1, (1, 2));
-            map.Add(item2);
-            map.Move(item, item2.Position);
-            Assert.Equal(2, map.Count);
-            Assert.Equal(2, map.GetItemsAt((1, 2)).Count());
-            Assert.Equal(item2.Position, item.Position);
-
             map.Remove(item);
             item.Position = (5, 6); // Attempt to validate handlers are unregistered (move would crash with an object not in the map)
-            Assert.Equal(1, map.Count);
+            Assert.Equal(0, map.Count);
         }
 
         [Fact]
         public void MapIsSyncedWhenPositionChangedTriggers()
         {
-            var map = new AutoSyncMultiSpatialMap<MockPositionableSpatialMapItem>();
+            var map = new AutoSyncSpatialMap<MockPositionableSpatialMapItem>();
             var item = new MockPositionableSpatialMapItem(1, (1, 2));
             item.PositionChanged += (s, e)
                 => Assert.Equal(e.NewValue, map.GetPositionOf((MockPositionableSpatialMapItem)s!));
@@ -75,7 +61,5 @@ namespace SadRogue.Primitives.UnitTests.SpatialMaps
             item.Position = (3, 4);
             map.Move(item, (2, 3));
         }
-
-
     }
 }

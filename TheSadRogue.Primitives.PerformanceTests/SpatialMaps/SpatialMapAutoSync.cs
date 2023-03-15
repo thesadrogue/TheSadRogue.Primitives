a57e991ce71a81c6dfc9a3d966358babb36af5a9
Chris3606 <cridley106@gmail.com>
2023-03-14 22:28:18 -0500

    Added performance tests for the AutoSyncSpatialMap implementation.

---
+++ TheSadRogue.Primitives.PerformanceTests/SpatialMaps/SpatialMapAutoSync.cs
@@ -0,0 +1,64 @@
+﻿using BenchmarkDotNet.Attributes;
+using SadRogue.Primitives;
+using SadRogue.Primitives.SpatialMaps;
+
+namespace TheSadRogue.Primitives.PerformanceTests.SpatialMaps;
+
+public class SpatialMapAutoSync
+{
+    private readonly Point _initialPosition = (0, 1);
+    private readonly Point _moveToPosition = (5, 6);
+    private readonly Point _addPosition = (1, 1);
+    private readonly IDPositionObject _trackedObject = new();
+    private readonly int _width = 10;
+    private AutoSyncSpatialMap<IDPositionObject> _testMap = null!;
+
+    [Params(1, 10, 50, 100)]
+    public int NumEntities;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _testMap = new AutoSyncSpatialMap<IDPositionObject> { { _trackedObject, _initialPosition } };
+
+        // Put other entities on the map, steering clear of the three points we need to remain clear to support
+        // benchmarked adds/removes.
+        int idx = -1;
+        while (_testMap.Count < NumEntities)
+        {
+            idx += 1;
+
+            if (idx == _initialPosition.ToIndex(_width) || idx == _moveToPosition.ToIndex(_width) || idx == _addPosition.ToIndex(_width)) continue;
+
+            var obj = new IDPositionObject { Position = Point.FromIndex(idx, _width) };
+            _testMap.Add(obj);
+        }
+    }
+
+    [Benchmark]
+    public int MoveTwice()
+    {
+        _testMap.Move(_trackedObject, _moveToPosition);
+        _testMap.Move(_trackedObject, _initialPosition); // Move it back to not spoil next benchmark
+        return _testMap.Count; // Ensure nothing is optimized out
+    }
+
+    [Benchmark]
+    public int MoveTwiceUsingPositionField()
+    {
+        _trackedObject.Position = _moveToPosition;
+        _trackedObject.Position = _initialPosition; // Move it back to not spoil next benchmark
+
+        return _testMap.Count; // Ensure nothing is optimized out
+    }
+
+    [Benchmark]
+    public int MoveAllTwice()
+    {
+        _testMap.MoveAll(_initialPosition, _moveToPosition);
+        // Move it back to not spoil next benchmark.  Valid since the GlobalSetup function used for this benchmark
+        // doesn't put anything at _moveToPosition in the initial state.
+        _testMap.MoveAll(_moveToPosition, _initialPosition);
+        return _testMap.Count; // Ensure nothing is optimized out
+    }
+}

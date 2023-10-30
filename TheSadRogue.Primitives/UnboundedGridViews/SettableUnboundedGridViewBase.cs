ff7aef0192a78eabb117285bc82f0bca1f6f65a8
Chris3606 <cridley106@gmail.com>
2023-10-29 21:44:19 -0500

    Added base classes, translation view, lambda view, and lambda translation view equivalents for unbounded grid views.

---
+++ TheSadRogue.Primitives/UnboundedGridViews/SettableUnboundedGridViewBase.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+
+namespace SadRogue.Primitives.UnboundedGridViews
+{
+    /// <summary>
+    /// A convenient base class to inherit from when implementing <see cref="ISettableUnboundedGridView{T}"/> that minimizes
+    /// the number of items you must implement by implementing indexers in terms of a single indexer taking a Point.
+    /// </summary>
+    /// <typeparam name="T">The type of value being returned by the indexer functions.</typeparam>
+    [PublicAPI]
+    public abstract class SettableUnboundedGridViewBase<T> : ISettableUnboundedGridView<T>
+    {
+        /// <inheritdoc cref="ISettableUnboundedGridView{T}"/>
+        public T this[int x, int y]
+        {
+            get => this[new Point(x, y)];
+            set => this[new Point(x, y)] = value;
+        }
+
+        /// <inheritdoc cref="ISettableUnboundedGridView{T}"/>
+        public abstract T this[Point pos] { get; set; }
+    }
+}
---
+++ TheSadRogue.Primitives/UnboundedGridViews/UnboundedGridViewBase.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace SadRogue.Primitives.UnboundedGridViews
+{
+    /// <summary>
+    /// A convenient base class to inherit from when implementing <see cref="IUnboundedGridView{T}"/> that minimizes
+    /// the number of items you must implement by implementing indexers in terms of a single indexer taking a Point.
+    /// </summary>
+    /// <typeparam name="T">The type of value being returned by the indexer functions.</typeparam>
+    [PublicAPI]
+    public abstract class UnboundedGridViewBase<T> : IUnboundedGridView<T>
+    {
+        /// <inheritdoc />
+        public T this[int x, int y] => this[new Point(x, y)];
+
+        /// <inheritdoc />
+        public abstract T this[Point pos] { get; }
+    }
+}
